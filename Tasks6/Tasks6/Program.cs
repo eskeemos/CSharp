b7b58fdf869d52531394fc9c6e98a032e0c82c6a
Łukasz Michalik <l.michalik004@gmail.com>
2021-05-07 11:17:46 +0200

    Work with classess and structures

--- Tasks6/Tasks6/Program.cs
+++ Tasks6/Tasks6/Program.cs
@@ -14,17 +14,17 @@ namespace Tasks6
         public static void najwiekszy(Prostokat[] tab)
         {
             double najwiekszy = 0; ;
-            for(int i = 0;i < tab.Length; i++)
+            for (int i = 0; i < tab.Length; i++)
             {
-                if(tab[i].powierzchnia() > najwiekszy) najwiekszy = tab[i].powierzchnia();
+                if (tab[i].powierzchnia() > najwiekszy) najwiekszy = tab[i].powierzchnia();
             }
             Console.WriteLine("Największa powierzchnia : {0}", najwiekszy);
         }
-        private double powierzchnia() 
+        private double powierzchnia()
         {
             return dlugosc * szerokosc;
         }
-        private double obwod() 
+        private double obwod()
         {
             return 2 * (dlugosc + szerokosc);
         }
@@ -42,21 +42,21 @@ namespace Tasks6
             initConsumption = initC;
             CurrConsumption = currC;
         }
-        public void GetData() 
+        public void GetData()
         {
             Console.WriteLine("Stan początkowy : {0}\nStan Bieżący : {1}", initConsumption, CurrConsumption);
         }
         public void EnergyUsed()
         {
             double EConsumed = CurrConsumption - initConsumption;
-            Console.WriteLine("Energy consumed : {0}\n",EConsumed);
+            Console.WriteLine("Energy consumed : {0}\n", EConsumed);
         }
     }
-    class Punkt 
+    class Punkt
     {
         public int x;
         public int y;
-        public Punkt(params int[] pkt) 
+        public Punkt(params int[] pkt)
         {
             x = pkt[0];
             y = pkt[1];
@@ -66,38 +66,99 @@ namespace Tasks6
             x += pkt[0];
             y += pkt[1];
         }
-        public void Wyświetl() 
+        public void Wyświetl()
         {
-            Console.WriteLine("Aktulne współrzędne : P({0},{1})",x,y);
+            Console.WriteLine("Aktulne współrzędne : P({0},{1})", x, y);
         }
-        public static void NaProstej(Punkt[] pkts) 
+        public static void NaProstej(Punkt[] pkts)
         {
             int Cy = pkts[0].y - pkts[0].x;
             int prawda = 1;
-            for(int i = 0;i < pkts.Length; i++)
+            for (int i = 0; i < pkts.Length; i++)
             {
-                if (pkts[i].x + Cy != pkts[i].y) 
+                if (pkts[i].x + Cy != pkts[i].y)
                 {
                     Console.WriteLine("Punkty nie są współliniowe.");
                     prawda = 0; break;
                 }
             }
-            if(prawda == 1) Console.WriteLine("Punkty są współliniowe.");
+            if (prawda == 1) Console.WriteLine("Punkty są współliniowe.");
         }
     }
-    class Odcinek 
+    class Odcinek
     {
-        int x ,y;
+        int x, y;
         double wynik;
-        public Odcinek(Punkt p1, Punkt p2) 
+        public Odcinek(Punkt p1, Punkt p2)
         {
             x = p1.x - p2.x;
             y = p1.y - p2.y;
         }
-        public void Długość() 
+        public void Długość()
         {
             wynik = Math.Sqrt(Math.Pow(y, 2) + Math.Pow(x, 2));
             Console.WriteLine("Dłogość odcinka : {0:.##}", wynik);
+        }
+    }
+    class Prostopadloscian
+    {
+        double dlugosc, szerokosc, wysokosc;
+        public Prostopadloscian(double a, double b, double c)
+        {
+            dlugosc = a;
+            szerokosc = b;
+            wysokosc = c;
+        }
+        public double Objetosc()
+        {
+            return dlugosc * szerokosc * wysokosc;
+        }
+        public static void Porowannanie2Obj(double obj1, double obj2)
+        {
+            string helpVar;
+            if (obj1 > obj2) helpVar = "pierwszym";
+            else helpVar = "drugim";
+            Console.WriteLine("Prostopadłoscian podany w {0} argumencie ma większą objętość.", helpVar);
+        }
+    }
+    struct Prostokat_
+    {
+        double dlugosc,
+               szerokosc;
+        public Prostokat_(double a, double b)
+        {
+            dlugosc = a;
+            szerokosc = b;
+        }
+        private double powierzchnia()
+        {
+            return dlugosc * szerokosc;
+        }
+        private double obwod()
+        {
+            return 2 * (dlugosc + szerokosc);
+        }
+        public void Prezentuj()
+        {
+            Console.Write("Powierzchnia : {0}\nObwód : {1}\n", powierzchnia(), obwod());
+        }
+    }
+    struct KandydatNaStudia
+    {
+        public string nazwisko;
+        public double punktyMatematyka,
+            punktyInformatyka,
+            punktyJezykObcy;
+        public KandydatNaStudia(string a, int b, int c, int d)
+        {
+            nazwisko = a;
+            punktyMatematyka = b;
+            punktyInformatyka = c;
+            punktyJezykObcy = d;
+        }
+        public double SumaPunktow()
+        {
+            return punktyMatematyka * .6 + punktyInformatyka * .5 + punktyJezykObcy * .2;
         }
     }
     class Program
@@ -129,6 +190,21 @@ namespace Tasks6
             // Zadanie 6.7 ----------------------------------------------------------
             Odcinek odc1 = new Odcinek(new Punkt(2,2), new Punkt(3,3));
             odc1.Długość();
+            // Zadanie 6.8 ----------------------------------------------------------
+            Prostopadloscian p1 = new Prostopadloscian(10, 20, 30);
+            Console.WriteLine("Objętosć p1 to : {0}", p1.Objetosc());
+            Prostopadloscian.Porowannanie2Obj(new Prostopadloscian(5, 10, 5).Objetosc(), new Prostopadloscian(6, 6, 6).Objetosc());
+            // Zadanie 6.9 ----------------------------------------------------------
+            Prostokat_[] tab = { new Prostokat_(2, 8), new Prostokat_(4, 6), new Prostokat_(5, 5) };
+            foreach (Prostokat_ x in tab) x.Prezentuj();
+            // Zadanie 6.10 ---------------------------------------------------------
+            KandydatNaStudia[] kandydat = { new KandydatNaStudia("Baranowski",55,89,100),
+                                            new KandydatNaStudia("Karolek",40,90,56),
+                                            new KandydatNaStudia("Wrobel",77,23,45)};
+            for(int i = 0;i < kandydat.Length;i++)
+            {
+                Console.WriteLine("Kandydat {0} uzyskał wynik : {1:.#}", kandydat[i].nazwisko, kandydat[i].SumaPunktow());
+            }
             */
         }
     }
