8ddf40f0db2b6f22ca0bf94fcf19ad7ececeb48a
Łukasz Michalik <l.michalik004@gmail.com>
2021-05-11 12:59:04 +0200

    Work with Database and redirect to main form

--- HotelSystemManagement/HotelSystemManagement/Form1.Designer.cs
+++ HotelSystemManagement/HotelSystemManagement/Form1.Designer.cs
@@ -32,8 +32,8 @@ namespace HotelSystemManagement
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
@@ -65,28 +65,28 @@ namespace HotelSystemManagement
             this.label2.TabIndex = 1;
             this.label2.Text = "MTS";
             // 
-            // textBox1
+            // username
             // 
-            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
-            this.textBox1.CausesValidation = false;
-            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(71, 120);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.PlaceholderText = "Username";
-            this.textBox1.Size = new System.Drawing.Size(200, 27);
-            this.textBox1.TabIndex = 2;
+            this.username.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.username.CausesValidation = false;
+            this.username.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.username.Location = new System.Drawing.Point(71, 120);
+            this.username.Name = "username";
+            this.username.PlaceholderText = "Username";
+            this.username.Size = new System.Drawing.Size(200, 27);
+            this.username.TabIndex = 2;
             // 
-            // textBox2
+            // password
             // 
-            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
-            this.textBox2.CausesValidation = false;
-            this.textBox2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox2.Location = new System.Drawing.Point(71, 171);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PlaceholderText = "Password";
-            this.textBox2.Size = new System.Drawing.Size(200, 27);
-            this.textBox2.TabIndex = 3;
-            this.textBox2.UseSystemPasswordChar = true;
+            this.password.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.password.CausesValidation = false;
+            this.password.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.password.Location = new System.Drawing.Point(71, 171);
+            this.password.Name = "password";
+            this.password.PlaceholderText = "Password";
+            this.password.Size = new System.Drawing.Size(200, 27);
+            this.password.TabIndex = 3;
+            this.password.UseSystemPasswordChar = true;
             // 
             // button1
             // 
@@ -101,6 +101,7 @@ namespace HotelSystemManagement
             this.button1.TabIndex = 4;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
@@ -136,14 +137,15 @@ namespace HotelSystemManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
             this.ClientSize = new System.Drawing.Size(300, 400);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -162,12 +164,13 @@ namespace HotelSystemManagement
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox U;
+        private System.Windows.Forms.TextBox password;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox username;
     }
 }
 
--- HotelSystemManagement/HotelSystemManagement/Form1.cs
+++ HotelSystemManagement/HotelSystemManagement/Form1.cs
@@ -7,15 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace HotelSystemManagement
 {
     public partial class Form1 : Form
     {
+        SqlConnection Con = new SqlConnection(@"Data Source=DESKTOP-DABF71C;Initial Catalog=hotel_db;Integrated Security=True");
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Con.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT COUNT(*) FROM Staff_tab " + "WHERE " +"StaffName = '"+ username.Text +"' AND StaffPassword = '" + password.Text + "'",Con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if(dt.Rows[0][0].ToString() == "1")
+            {
+                MainForm mf = new MainForm();
+                mf.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Wrong Username or Password. Try Again.");
+            }
+            Con.Close();
+        }
     }
 }
