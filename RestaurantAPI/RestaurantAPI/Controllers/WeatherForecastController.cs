0f3dc85b2b3ee80d88556d4a295583775e199804
Łukasz Michalik <l.michalik004@gmail.com>
2021-06-28 21:13:43 +0200

    Work with net core api

---
+++ RestaurantAPI/RestaurantAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IWeatherForecastService _service;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        [HttpGet("date")]
+        public ContentResult About()
+        {
+            return Content("An API listing authors of docs.asp.net.");
+        }
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var result = _service.Get();
+            return result;
+        }
+    }
+}
