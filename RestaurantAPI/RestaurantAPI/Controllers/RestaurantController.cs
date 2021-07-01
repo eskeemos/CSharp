0168d9b4bd4ccccc39baebb779cc072a36bfc686
Łukasz Michalik <l.michalik004@gmail.com>
2021-07-01 12:03:22 +0200

    Resource getting

---
+++ RestaurantAPI/RestaurantAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/restaurant")]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly RestaurantDbContext _dbContext;
+        public RestaurantController(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Restaurant>> GetAll()
+        {
+            var restaurants = _dbContext.Restaurants.ToList();
+            return Ok(restaurants);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Restaurant> GetSingle([FromRoute] int id)
+        {
+            var restaurant = _dbContext.Restaurants.FirstOrDefault((r) => r.ID == id);
+
+            if (restaurant is null) return NotFound();
+            return Ok(restaurant);
+        }
+    }
+}
