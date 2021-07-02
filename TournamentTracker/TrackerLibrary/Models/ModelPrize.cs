74dc6881501471c201b4c14ed57abe0209ac9534
Łukasz Michalik <l.michalik004@gmail.com>
2021-07-02 17:31:04 +0200

    Create models

---
+++ TournamentTracker/TrackerLibrary/Models/ModelPrize.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class ModelPrize
+    {
+        public int PlaceNumber { get; set; }
+        public int PlaceName { get; set; }
+        public int PrizeAmount { get; set; }
+        public int PrizePercentage { get; set; }
+    }
+}
---
+++ TournamentTracker/TrackerLibrary/Models/ModelTeam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class ModelTeam
+    {
+        public string TeamName { get; set; }
+        public List<ModelPerson> TeamMembers { get; set; } = new List<ModelPerson>();
+    }
+}
